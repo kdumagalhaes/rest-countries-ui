cb08c191e1b5cb7096a625b09add0ec4ee8cf6d3
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-04-07 11:54:07 +0100

    add color themes

---
+++ src/themes/themes.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+    element: 'hsl(0, 0%, 100%)',
+    background: 'hsl(0, 0%, 98%)',
+    text: 'hsl(200, 15%, 8%)',
+    input: 'hsl(0, 0%, 52%)'
+}
+
+export const darkTheme = {
+    element: 'hsl(209, 23%, 22%)',
+    background: 'hsl(207, 26%, 17%)',
+    text: 'hsl(0, 0%, 100%)',
+    input: 'hsl(0, 0%, 52%)'
+}
