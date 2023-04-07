95c56346268d475dc57fe29169f8fea964417686
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-04-07 14:24:37 +0100

    Add button cursor pointer in global styles

--- src/components/Header/ToggleButton/ToggleButton.tsx
+++ src/components/Header/ToggleButton/ToggleButton.tsx
@@ -1,3 +1,10 @@
+import { Container } from '../styles'
+
 export function ToggleButton() {
-  return <h2>toggle button</h2>
+  return (
+    <Container>
+      <img src="" alt="moon icon" className="moon-icon" />
+      Dark Mode
+    </Container>
+  )
 }
