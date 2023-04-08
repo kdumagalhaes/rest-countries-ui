fa83968279653bf17266226a5cbb4a16bc98ac45
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-04-08 18:47:25 +0100

    Add FilterSelector component

--- src/components/FilterSelector/FilterSelector.tsx
+++ src/components/FilterSelector/FilterSelector.tsx
@@ -1,3 +1,34 @@
+import { ChangeEvent } from 'react'
+import { useThemeSelection } from '../../context/ThemeContext/ThemeContext'
+import { Label } from './styles'
+
 export function FilterSelector() {
-  return <h2>filter selector</h2>
+  const { toggleSelection } = useThemeSelection()
+
+  const regions = [
+    'Filter by Region',
+    'Africa',
+    'America',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ]
+
+  const handleSelect = (event: ChangeEvent<HTMLSelectElement>) =>
+    console.log(event.target.value)
+
+  return (
+    <Label themeSelection={toggleSelection}>
+      Filter by Region
+      <select onChange={handleSelect} name="filterSelector">
+        {regions.map((region, index) => {
+          return (
+            <option key={index} value={region}>
+              {region}
+            </option>
+          )
+        })}
+      </select>
+    </Label>
+  )
 }
